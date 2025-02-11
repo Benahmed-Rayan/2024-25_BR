08d8b466e4e43d095e71a6ca220318ea2c5f0d3b
unknown <rayan080205@gmail.com>
2025-02-11 15:17:24 +0100

    save

--- exo6/Main.java
+++ exo6/Main.java
@@ -4,13 +4,13 @@ public class Main {
     
     static String[] cours = {"dev java", "management", "cyber"};
     
-    public static string getElement (int index) throws ArrayIndexOutOfBoundsException{
+    public static String getElement (int index) throws ArrayIndexOutOfBoundsException{
         return cours[index];
     }
     
     public static void main (String[] args){
         try{
-            System.out.println(getElement(index:6));
+            System.out.println(getElement(6));
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Erreur capt " + e.getMessage());
         } 
